26a6ca64cdd1ee44134abe062d152680aba0ddc5
mozg404 <masterdmx3325@gmail.com>
2025-10-24 19:40:53 +0500

    fix: add index for user_id in transaction table

--- database/migrations/2025_10_23_212155_create_transactions_table.php
+++ database/migrations/2025_10_23_212155_create_transactions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->index()->constrained();
             $table->string('type', 50);
             $table->decimal('amount', 15, 2);
             $table->foreignId('related_user_id')->nullable()->index()->constrained('users');
